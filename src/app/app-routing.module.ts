4342da477476d8524ec9ebaa51e72ac862487e4a
Dmitro Sekhno <muravytskyi.github.io>
2020-05-21 20:31:29 +0300

    build demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)
+    loadChildren: './layout/layout.module#LayoutModule'//() => import('./layout/layout.module').then(m => m.LayoutModule)
   },
 ];
 
